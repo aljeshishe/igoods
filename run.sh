48696104ea26d696173cecd9b3f32c533fb6e487
Aleksei.Grachev <aleksei_grachev@epam.com>
2020-03-15 22:04:00 +0300

    added airflow container name

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-docker run -d --restart=always -p 8080:8080 -v $(pwd)/requirements.txt:/requirements.txt  \
+docker run -d --restart=always -p 8080:8080
+    -t airflow
+    -v $(pwd)/requirements.txt:/requirements.txt  \
     -v $(pwd):/app -v $(pwd)/dags:/usr/local/airflow/dags \
     -v $(pwd)/airflow.cfg:/usr/local/airflow/airflow.cfg puckel/docker-airflow webserver
