45594eec2a354b2a66428e9a4bf3f64fbdaf8a57
Aleksei.Grachev <aleksei_grachev@epam.com>
2020-03-15 22:39:54 +0300

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker run -d --restart=always -p 8080:8080
-    -t airflow
+docker run -d --restart=always -p 8080:8080 \
+    --name airflow \
     -v $(pwd)/requirements.txt:/requirements.txt  \
     -v $(pwd):/app -v $(pwd)/dags:/usr/local/airflow/dags \
     -v $(pwd)/airflow.cfg:/usr/local/airflow/airflow.cfg puckel/docker-airflow webserver
