3064efd28988c8024ec63e7f7d41d9e8a5224e74
Aleksei.Grachev <aleksei_grachev@epam.com>
2020-03-21 15:34:15 +0300

    start at 6am

--- dags/igoods.py
+++ dags/igoods.py
@@ -27,7 +27,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 schedule_interval = timedelta(days=1)
-start_date = datetime.now(tz=pendulum.timezone('Europe/Moscow')).replace(hour=8, minute=0) - schedule_interval
+start_date = datetime.now(tz=pendulum.timezone('Europe/Moscow')).replace(hour=6, minute=0) - schedule_interval
 
 dag = DAG('igoods',
           catchup=False,
